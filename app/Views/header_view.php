3673d604b198904af6857793163390887692c650
Fandhiprayoga <43158792+Fandhiprayoga@users.noreply.github.com>
2024-07-28 09:32:41 +0700

    feat add description

--- app/Views/header_view.php
+++ app/Views/header_view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Payment Check ITTP</title>
-    <meta name="description" content="The small framework with powerful features">
+    <meta name="description" content="Apss for check yout payment status">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/logoittp.ico">
 
